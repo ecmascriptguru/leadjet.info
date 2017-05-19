3db3731ad6eb41cb41a8416b579694aa2b28f1b2
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-19 02:12:43 -0400

    voyager initialized

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Voyager
+         */
+        TCG\Voyager\VoyagerServiceProvider::class,
+
     ],
 
     /*
