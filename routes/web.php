afb01f9d8e0e6a6922f416ddef4dc6a5b90acb39
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-23 10:41:25 -0400

    job doesn't work yet

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/verifyemail/{token}', 'Auth\RegisterController@verify');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
