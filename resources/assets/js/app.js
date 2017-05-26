bafb982e8a2072fd6547f4278e8eba6a3a74651b
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-26 00:41:56 -0400

    converjs chart tried

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,7 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
+import CanvasJs from 'canvasjs';
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
